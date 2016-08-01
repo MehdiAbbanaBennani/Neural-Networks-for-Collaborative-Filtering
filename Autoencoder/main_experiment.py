8e96011043d3991762daeeea7c1afdd6fcbae062
MehdiAB161 <mehdi-b-@live.fr>
2016-08-01 18:59:39 +0200

    building Stability

---
+++ Autoencoder/main_experiment.py
@@ -0,0 +1,8 @@
+from Experiment import autoencoder_experiment
+
+autoencoder_experiment(nb_tests=40,
+                       mean_iterations=1,
+                       database=1,
+                       nb_epoch=15)
+
+
