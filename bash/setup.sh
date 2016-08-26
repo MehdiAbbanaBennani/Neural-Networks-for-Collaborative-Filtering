b8689e666598ac5999533c4197e69d4d37d10d3a
MehdiAB161 <mehdi-b-@live.fr>
2016-08-26 20:58:23 +0200

    Transpose and stability fixes

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/MehdiAB161/Neural-Networks-for-Collaborative-Filtering.git
-cd Neural-Network-Collaborative-Filtering/
+cd Neural-Network-for-Collaborative-Filtering/
 cd bash
 ./import.sh 0
 ./import.sh 1
 ./import.sh 2
+echo 'Installation complete'
 
 
