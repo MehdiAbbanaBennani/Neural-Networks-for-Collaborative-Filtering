f3f5984da43ce9267ee721d4de05cfae86bfedb6
MehdiAB161 <mehdi-b-@live.fr>
2016-08-26 20:39:16 +0200

    Transpose and stability fixes

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 sed -i '1s/.*/from config.autoencoder.V_1M import Experiment/' main.py
-sed -i '5s/.*/#SBATCH --job-name=AV1C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LAV1C/' job.slurm
 sbatch job.slurm
 
 sed -i '1s/.*/from config.autoencoder.U_10M import Experiment/' main.py
-sed -i '5s/.*/#SBATCH --job-name=AU10C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LAU10C/' job.slurm
 sbatch job.slurm
 
 sed -i '1s/.*/from config.autoencoder.V_10M import Experiment/' main.py
-sed -i '5s/.*/#SBATCH --job-name=AV10C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LAV10C/' job.slurm
 sbatch job.slurm
 
 sed -i '1s/.*/from config.stability.U_1M import Experiment/' main.py
-sed -i '5s/.*/#SBATCH --job-name=SU1C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LSU1C/' job.slurm
+sbatch job.slurm
+
+sed -i '1s/.*/from config.stability.U_10M import Experiment/' main.py
+sed -i '5s/.*/#SBATCH --job-name=LSU1C/' job.slurm
 sbatch job.slurm
 
 sed -i '1s/.*/from config.stability.V_1M import Experiment/' main.py
-sed -i '5s/.*/#SBATCH --job-name=SV1C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LSV1C/' job.slurm
 sbatch job.slurm
 
 sed -i '1s/.*/from config.extra.U_1M import Experiment' main.py
-sed -i '5s/.*/#SBATCH --job-name=EU1C/' job.slurm
+sed -i '5s/.*/#SBATCH --job-name=LEU1C/' job.slurm
 sbatch job.slurm
